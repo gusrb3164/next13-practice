eecb4942772d2610ebd2d225d53ec72005dd5064
gusrb3164 <gusrb3164@gmail.com>
2023-06-30 11:34:43 +0900

    week 1

---
+++ app/components/Price.tsx
@@ -0,0 +1,25 @@
+import {PRICE} from '@prisma/client';
+import React from 'react';
+
+const Price = ({price}: {price: PRICE}) => {
+  const renderPrice = () => {
+    if (price === 'CHEAP') {
+      return (
+        <>
+          <span>$$</span> <span className="text-gray-400">$$</span>
+        </>
+      );
+    } else if (price === 'REGULAR') {
+      return (
+        <>
+          <span>$$$</span> <span className="text-gray-400">$</span>
+        </>
+      );
+    } else {
+      return <span>$$$$</span>;
+    }
+  };
+  return <p className="flex mr-3">{renderPrice()}</p>;
+};
+
+export default Price;
---
+++ app/reserve/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import {NextPage} from 'next';
+import React from 'react';
+import Form from './components/Form';
+import Header from './components/Header';
+
+const ReservationPage: NextPage = () => {
+  return (
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
+  );
+};
+
+export default ReservationPage;
